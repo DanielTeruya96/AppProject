d63275c2a3a3d14c4af9f5414a149bb6a1f2df62
Artur Duarte <arturduarte10@hotmail.com>
2016-08-05 13:46:54 -0400

    Atualizacao / telas e Botao
    NforGot02

---
+++ NforGot-art-02/NforGot/app/src/main/java/ufms/cpcx/artcomp/nforgot/CadastreActivity.java
@@ -0,0 +1,49 @@
+package ufms.cpcx.artcomp.nforgot;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CadastreActivity extends Activity {
+    private EditText nome;
+    private EditText email;
+    private EditText chave;
+    private Button cadastro;
+    private final String arq = "arqPreferencia";
+    Context context = this;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_cadastre);
+
+        nome = (EditText) findViewById(R.id.editNome);
+        email = (EditText) findViewById(R.id.edtEmail);
+        chave = (EditText) findViewById(R.id.edtChave);
+        cadastro = (Button) findViewById(R.id.btnCadastro);
+        cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sh = getSharedPreferences(arq,0);
+                SharedPreferences.Editor editor = sh.edit();
+                editor.putString("nome",nome.getText().toString());
+                editor.putString("email",email.getText().toString());
+                editor.putString("chave",chave.getText().toString());
+                editor.putBoolean("cadastrado",true);
+                editor.commit();
+
+                startActivity(new Intent(context,ChaveActivity.class));
+                finish();
+
+            }
+        });
+    }
+}
---
+++ NforGot-art-02/NforGot/app/src/main/java/ufms/cpcx/artcomp/nforgot/ChaveActivity.java
@@ -0,0 +1,15 @@
+package ufms.cpcx.artcomp.nforgot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ChaveActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_chave);
+    }
+}
